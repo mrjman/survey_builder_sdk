4cd4ca7b4d2b786ffec127182703fad7971962a4
Jesse Manning <jesse@mondorobot.com>
2020-02-03 12:56:23 -0700

    update Request class to store both service and service_klass

--- lib/survey_builder_sdk/resources/request.rb
+++ lib/survey_builder_sdk/resources/request.rb
@@ -1,20 +1,21 @@
 module SurveyBuilder
   class Request
-    attr_accessor :client, :method, :service, :args, :params, :options
+    attr_accessor :client, :method, :service, :service_klass, :args, :params, :options
 
     def initialize(client, service, method, options = {})
       @client = client
-      @service = client.send(service)
+      @service = service
+      @service_klass = client.send(service)
       @method = method
       @options = options
-      @args = SurveyBuilder::Util.array_wrap(options[:args])
+      @args = Util.array_wrap(options[:args])
       @params = options.dup.tap { |hash| hash.delete(:args) }
     end
 
     def call
-      data = service.send(method, *request_args)
+      data = service_klass.send(method, *request_args)
 
-      SurveyBuilder::Response.new(self, data)
+      Response.new(self, data)
     end
 
     private
